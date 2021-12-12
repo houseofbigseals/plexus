7c3c0b39dcace5f7e1ac5fedc8c90791548d4031
greencrow <slonik256@yandex.ru>
2021-12-12 05:39:52 +0300

    command images added

--- devices/rpi_gpio_relay_device.py
+++ devices/rpi_gpio_relay_device.py
@@ -8,6 +8,7 @@ try:
     from utils.logger import PrintLogger
     from low_level_drivers.rpi_gpio_relay_driver import RpiGpioChannelHandler
     from nodes.message import Message
+    from nodes.command import Command
     from devices.base_device import BaseDevice
 except Exception:
     # here we trying to manually add our lib path to python path
@@ -15,6 +16,8 @@ except Exception:
     sys.path.insert(0, "{}/utils".format(abspath))
     sys.path.insert(0, "{}/low_level_drivers".format(abspath))
     sys.path.insert(0, "{}/devices".format(abspath))
+    sys.path.insert(0, "{}/nodes".format(abspath))
+    from command import Command
     from message import Message
     from logger import PrintLogger
     from message import Message
@@ -32,7 +35,20 @@ class RpiGpioRelayDevice(BaseDevice):
         self._pin = pin_name
         self._relay = RpiGpioChannelHandler(pin_name)
         self._description = "this is simple test device to control one relay channel"
-        self._available_commands.extend(["on", "off"])
+
+        on_command = Command(
+            name="on",
+            annotation="set relay high",
+            output_kwargs={"ack_str": "ack"}
+        )
+
+        off_command = Command(
+            name="off",
+            annotation="set relay low",
+            output_kwargs={"ack_str": "ack"}
+        )
+
+        self._available_commands.extend([on_command, off_command])
         self._state = "off"
 
     def device_commands_handler(self, command, **kwargs):
