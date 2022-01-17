c7cdce53546daf9d54374c8fbc4d04f04b864de4
greencrow <slonik256@yandex.ru>
2022-01-17 19:01:19 +0300

    added si7021 sensor device

--- devices/si7021_device.py
+++ devices/si7021_device.py
@@ -1,9 +1,9 @@
 
-import subprocess
+
 import time, sys, os
 try:
     from devices.base_device import BaseDevice
-    from low_level_drivers.bmp180_driver import get_bmp180_data
+    from low_level_drivers.si7021_driver import get_si7021_data
     from nodes.command import Command
 except ModuleNotFoundError:
     # here we trying to manually add our lib path to python path
@@ -13,18 +13,18 @@ except ModuleNotFoundError:
     sys.path.insert(0, "{}/nodes".format(abspath))
     from command import Command
     from base_device import BaseDevice
-    from bmp180_driver import get_bmp180_data
+    from si7021_driver import get_si7021_data
 
 
-class BMP180Sensor(BaseDevice):
+class SI7021(BaseDevice):
     """
 
     """
     def __init__(self, name):
         super().__init__(name)
         # manually add here important variables and commands
-        self.bmp_180_state = "None"
-        self._description = "this is simple test device to control simple sensor"
+        # self.si7021_state = "None"
+        self._description = " SI7021 temp and humidity handler device "
 
         get_state_command = Command(
             name="get_state",
@@ -32,61 +32,61 @@ class BMP180Sensor(BaseDevice):
             output_kwargs={"state": "str"}
         )
 
-        get_temperature_command = Command(
-            name="get_temperature",
-            annotation="get_temperature",
-            output_kwargs={"temp": "float"}
-        )
-
-        get_pressure_command = Command(
-            name="get_pressure",
-            annotation="get_pressure",
-            output_kwargs={"pressure": "float"}
-        )
-
-        get_temp_and_press_command = Command(
-            name="get_temp_and_press",
-            annotation="get_temp_and_press",
+        get_temp_and_hum_command = Command(
+            name="get_temp_and_hum",
+            annotation="get temp and hum data",
             output_kwargs={"temp and pressure": "tuple"}
         )
 
-        self._available_commands.extend([get_pressure_command, get_temperature_command,
-                                         get_temp_and_press_command, get_state_command])
+        get_hum_command = Command(
+            name="get_hum",
+            annotation="get hum data",
+            output_kwargs={"hum": "float"}
+        )
+
+        get_temp_command = Command(
+            name="get_temp",
+            annotation="get temp data",
+            output_kwargs={"temp": "float"}
+        )
+
+        self._available_commands.extend([get_temp_and_hum_command, get_hum_command,
+                                         get_temp_command, get_state_command])
 
     def device_commands_handler(self, command, **kwargs):
         if command == "get_state":
             print("command == 'get_state'")
-            # do kill program: blink 5 times in 1 secs
             return self._status
-        if command == "get_temperature":
+
+        if command == "get_temp":
             try:
-                temp, pressure, altitude = get_bmp180_data()
+                cels_temp, humidity = get_si7021_data()
                 self._status = "works"
-                return temp
-            except Exception as e:
-                self._status = "error"
-                return e
-        if command == "get_pressure":
-            try:
-                temp, pressure, altitude = get_bmp180_data()
-                self._status = "works"
-                return pressure
+                return cels_temp
             except Exception as e:
                 self._status = "error"
                 return e
 
-        if command == "get_temp_and_press":
+        if command == "get_hum":
             try:
-                temp, pressure, altitude = get_bmp180_data()
+                cels_temp, humidity = get_si7021_data()
                 self._status = "works"
-                return temp, pressure
+                return humidity
             except Exception as e:
                 self._status = "error"
                 return e
 
+        if command == "get_temp_and_hum":
+            try:
+                cels_temp, humidity = get_si7021_data()
+                self._status = "works"
+                return cels_temp, humidity
+            except Exception as e:
+                self._status = "error"
+                return e
 
 if __name__ == "__main__":
-    c = BMP180Sensor("bmp1")
+    c = SI7021("si7021")
     print(c.call("info"))
     print(c.call("get_state"))
     while True:
@@ -94,4 +94,5 @@ if __name__ == "__main__":
         time.sleep(1)
         print(c.call("get_temp_and_press"))
         time.sleep(1)
-    # c.call("start")
+        print(c.call("get_state"))
+        time.sleep(1)
